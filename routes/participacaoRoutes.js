33024328076083d70de852424168e3e936876371
Felipe <felipegustavo042009@gmail.com>
2025-10-09 20:56:43 -0300

    Delete routes directory

--- routes/participacaoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const participacaoController = require("../controllers/participacaoController");
-
-router.post("/", participacaoController.createAtividade);
-router.get("/sala/:sala_id", participacaoController.getAtividadesBySala);
-router.delete("/:id", participacaoController.deleteAtividade);
-
-module.exports = router;
