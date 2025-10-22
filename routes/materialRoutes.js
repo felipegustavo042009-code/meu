40840a9901b50880a771f42ac890e30572ffac3f
Felipe <felipegustavo042009@gmail.com>
2025-10-22 06:03:52 -0300

    Delete routes directory

--- routes/materialRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const materialController = require("../controllers/materialController");
-
-router.post("/", materialController.createMaterial);
-router.get("/sala/:sala_id", materialController.getMateriaisBySala);
-router.delete("/:id", materialController.deleteMaterial);
-
-module.exports = router;
