dd2489fb16a7b74f81471d65caa56aa0f86c6fb0
Felipe <felipegustavo042009@gmail.com>
2025-10-22 06:04:27 -0300

    Delete supabase/migrations directory

--- supabase/migrations/20251008161138_update_salas_professor_optional.sql
+++
@@ -1,9 +0,0 @@
-/*
-  # Tornar professor_id opcional
-
-  1. Alterações
-    - Permitir professor_id como NULL na tabela salas
-    - Isso permite criar salas sem necessariamente ter um ID de professor UUID válido
-*/
-
-ALTER TABLE salas ALTER COLUMN professor_id DROP NOT NULL;
